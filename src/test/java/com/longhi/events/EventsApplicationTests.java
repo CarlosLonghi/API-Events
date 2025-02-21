7a3e422270a86361df7c6a5c537a2c47029795eb
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-02-21 20:50:23 -0300

    Initial Commit

---
+++ src/test/java/com/longhi/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longhi.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
